df4c1de51507075ec6cdea720857d41b92a89959
Maddy Dannenberg <mmdberg@gmail.com>
2018-03-25 14:33:12 -0600

    Add images to vehicle cards

--- src/helpers/card-images.js
+++ src/helpers/card-images.js
@@ -20,28 +20,54 @@ import img18 from './peopleImages/r5-d4.jpeg';
 import img19 from './peopleImages/biggs.jpeg';
 import img20 from './peopleImages/obi-wan.jpeg';
 
-export const planetImages = {
-'Alderaan': img1,
-'Yavin IV': img2,
-'Hoth': img3,
-'Dagobah': img4,
-'Bespin': img5,
-'Endor': img6,
-'Naboo': img7,
-'Coruscant': img8,
-'Kamino': img9,
-'Geonosis': img10
-}
+import img21 from './vehicleImages/Sandcrawler.jpeg';
+import img22 from './vehicleImages/T-16_skyhopper.png';
+import img23 from './vehicleImages/X34-landspeeder.jpg';
+import img24 from './vehicleImages/TIE_LN_starfighter.jpeg';
+import img25 from './vehicleImages/snowspeeder.jpeg';
+import img26 from './vehicleImages/tie_bomber.jpeg';
+import img27 from './vehicleImages/at_at.jpg';
+import img28 from './vehicleImages/AT-ST.jpeg';
+import img29 from './vehicleImages/cloud_car.jpg';
+import img30 from './vehicleImages/sail_barge.jpeg';
 
 export const peopleImages = {
-'Luke Skywalker': img11,
-'C-3PO': img12,
-'R2-D2': img13,
-'Darth Vader': img14,
-'Leia Organa': img15,
-'Owen Lars': img16,
-'Beru Whitesun lars': img17,
-'R5-D4': img18,
-'Biggs Darklighter': img19,
-'Obi-Wan Kenobi': img20
-}
+  'Luke Skywalker': img11,
+  'C-3PO': img12,
+  'R2-D2': img13,
+  'Darth Vader': img14,
+  'Leia Organa': img15,
+  'Owen Lars': img16,
+  'Beru Whitesun lars': img17,
+  'R5-D4': img18,
+  'Biggs Darklighter': img19,
+  'Obi-Wan Kenobi': img20
+};
+
+export const planetImages = {
+  'Alderaan': img1,
+  'Yavin IV': img2,
+  'Hoth': img3,
+  'Dagobah': img4,
+  'Bespin': img5,
+  'Endor': img6,
+  'Naboo': img7,
+  'Coruscant': img8,
+  'Kamino': img9,
+  'Geonosis': img10
+};
+
+export const vehicleImages = {
+  'Sand Crawler': img21,
+  'T-16 skyhopper': img22,
+  'X-34 landspeeder': img23,
+  'TIE/LN starfighter': img24,
+  'Snowspeeder': img25,
+  'TIE bomber': img26,
+  'AT-AT': img27,
+  'AT-ST': img28,
+  'Storm IV Twin-Pod cloud car': img29,
+  'Sail barge': img30
+};
+
+
